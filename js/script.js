746d9529925969b7d1f28eaf4d9e253316aed1ba
Gius993 <giuseppevidili9@gmail.com>
2022-06-06 16:47:19 +0200

    prezzo completo

--- js/script.js
+++ js/script.js
@@ -9,11 +9,20 @@ generateButton.addEventListener('click',
 		
 		//prezzo
 		const price= userKm * 0.21;
-		
-		
+		let discount = 0;
+		//if
+		if(userAge==='minorenne') {
+			discount = price * 20 / 100;
+		} else if (userAge === 'over') {
+			discount = price * 40 / 100;
+		}
+		const finalPrice = price - discount;
+
+		//consolelog
 		console.log(userName);
 		console.log(userKm);
-		console.log(userAge)
+		console.log(userAge);
+		console.log(finalPrice);
 
 	}
 );
