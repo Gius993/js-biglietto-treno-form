a459deb1e74ec14370bdb556908b4441d1bd32ff
Gius993 <giuseppevidili9@gmail.com>
2022-06-06 16:30:58 +0200

    sezione età completate

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+//genrate ticket
 const generateButton = document.getElementById('generate-ticket');
 generateButton.addEventListener('click',
 	function() {
+		
 		const userName = document.getElementById('user-name').value;
-	
+		const userKm = document.getElementById('user-km').value;
+		const userAge = document.getElementById('user-age').value;	
+		
+		//prezzo
+		const price= userKm * 0.21;
+		
+		
 		console.log(userName);
+		console.log(userKm);
+		console.log(userAge)
+
 	}
 );
+
